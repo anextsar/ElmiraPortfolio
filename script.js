735b8300b5ddf0c5cb5ee13647f37a69649385cd
anextsar <anextsar7050@gmail.com>
2025-01-12 23:27:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Add a smooth scroll effect for navbar links
+document.querySelectorAll('.navbar a').forEach(link => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute('href').substring(1);
+      const targetSection = document.getElementById(targetId);
+      window.scrollTo({
+        top: targetSection.offsetTop,
+        behavior: 'smooth',
+      });
+    });
+  });
+  
